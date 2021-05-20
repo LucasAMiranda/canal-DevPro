b37e18d9dae423f55bf3133aa71301ef182a3759
LucasAMiranda <lucasa.miranda@hotmail.com>
2021-05-20 18:12:31 -0300

    aula3 - completada com sucesso

--- devpro/devpro/encurtador/views.py
+++ devpro/devpro/encurtador/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, redirect
 
 from devpro.encurtador.models import UrlRedirect
 
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    context = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
+
+    }
+    return render(requisicao, 'encurtador/relatorio.html', context)
+
 def redirecionar(requisicao, slug):
-    urlRedirect = UrlRedirect.objects.get(slug=slug)
-    return redirect(urlRedirect.destino)
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    return redirect(url_redirect.destino)
